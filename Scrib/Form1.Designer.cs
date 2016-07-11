313768238f89ae473fd9460c72a40b8dfa57ce84
coderpadm <padmavathy.sivakumaran@gmail.com>
2016-07-11 10:45:00 -0700

    Illustrate performance issues

--- Scrib/Form1.Designer.cs
+++ Scrib/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(278, 244);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
--- Scrib/Form1.cs
+++ Scrib/Form1.cs
@@ -12,6 +12,10 @@ namespace Scrib
 {
     public partial class Form1 : Form
     {
+        Point mouse;
+
+        List<Point> points = new List<Point>();
+        
         public Form1()
         {
             InitializeComponent();
@@ -19,11 +23,33 @@ namespace Scrib
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.DrawRectangle(Pens.Green, 10, 20,100,50);
+            //For single point
+            if(mouse!=null)
+            {
+                e.Graphics.DrawRectangle(Pens.Green, 10, 20, 100, 50);
+            }
+
+            //For multiple points - to illustrate perfomance issues
+            foreach (Point point in points)
+            {
+                e.Graphics.DrawRectangle(Pens.Green, point.X, point.Y, 100, 50);
+            }
 
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //For one location
+            mouse = e.Location;
+
+            //For multiple points
+            points.Add(e.Location);
+
+            //Please call paint as soon as possible
+            Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
